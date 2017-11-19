3825b91801caaba3c6c7fc84bf8882fe52726e44
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2017-11-19 23:07:50 +0100

    Introducing boilerplates

--- bin/commands/boil.js
+++ bin/commands/boil.js
@@ -1,5 +1,33 @@
 const chalk = require('chalk');
+const { execSync } = require('child_process');
+const fs = require('fs');
 
 module.exports = (webpackConfig, webpackWizardConfig, argv) => {
-  console.log(chalk.yellow('Boiling.', argv));
+  const { boilerplate } = argv;
+  console.log(chalk.yellow(`Boiling "${boilerplate}".`));
+  installDependencies(boilerplate);
+  installDevDependencies(boilerplate);
+  console.log(chalk.green(`Boiling "${boilerplate}" complete.`));
+};
+
+const installDependencies = createDependenciesInstaller('dependencies.json', '--save');
+const installDevDependencies = createDependenciesInstaller('dev-dependencies.json', '--save-dev');
+
+const createDependenciesInstaller = (filename, npmInstallParam) => (boilerplate) => {
+  const dependenciesPath = `./boilerplates/${boilerplate}/${filename}`;
+  const dependencies = readDependencies(dependenciesPath);
+  if (dependencies.length > 0) {
+    execSync(`npm install ${npmInstallParam} ${dependencies.join(' ')}`);
+  }
+};
+
+const readDependencies = (dependenciesPath) => {
+  const fileExists = fs.existsSync(dependenciesPath);
+  const dependenciesJson = fileExists ? require(dependenciesPath) : {};
+  return Object.keys(dependenciesJson).reduce(
+    (list, dependency) => list.concat([
+      `${dependency}@${dependenciesJson[dependency]}`
+    ]),
+    []
+  );
 };
