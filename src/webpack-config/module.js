b1dc4b09e4e4782d0e8bae978c08c3872622f9d1
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-08-31 17:09:54 +0200

    0.1.0-beta.24 removing babel stage-0 preset

--- src/webpack-config/module.js
+++ src/webpack-config/module.js
@@ -136,9 +136,32 @@ const createJsRules = (wizardConfig) => ({
     loader: 'babel-loader',
     options: {
       presets: [
-        [ require.resolve('@babel/preset-env') ],
-        [ require.resolve('@babel/preset-react') ],
-        [ require.resolve('@babel/preset-stage-0'), { decoratorsLegacy: true } ]
+        require.resolve('@babel/preset-env'),
+        require.resolve('@babel/preset-react'),
+
+        // Stage 0
+        require.resolve('@babel/plugin-proposal-function-bind'),
+
+        // Stage 1
+        require.resolve('@babel/plugin-proposal-export-default-from'),
+        require.resolve('@babel/plugin-proposal-logical-assignment-operators'),
+        [ require.resolve('@babel/plugin-proposal-optional-chaining'), { "loose": false } ],
+        [ require.resolve('@babel/plugin-proposal-pipeline-operator'), { "proposal": "minimal" } ],
+        [ require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'), { "loose": false } ],
+        require.resolve('@babel/plugin-proposal-do-expressions',
+
+        // Stage 2
+        [ require.resolve('@babel/plugin-proposal-decorators'), { "legacy": true } ],
+        require.resolve('@babel/plugin-proposal-function-sent'),
+        require.resolve('@babel/plugin-proposal-export-namespace-from'),
+        require.resolve('@babel/plugin-proposal-numeric-separator'),
+        require.resolve('@babel/plugin-proposal-throw-expressions'),
+
+        // Stage 3
+        require.resolve('@babel/plugin-syntax-dynamic-import'),
+        require.resolve('@babel/plugin-syntax-import-meta'),
+        [ require.resolve('@babel/plugin-proposal-class-properties'), { "loose": false } ],
+        require.resolve('@babel/plugin-proposal-json-strings')
       ],
       plugins: [
         require.resolve('react-hot-loader/babel')
