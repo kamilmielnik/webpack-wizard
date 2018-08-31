7f6bca6453fb0bb22c1c6d2df506508c8ed7bdc8
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-08-31 17:45:28 +0200

    0.1.0-beta.25 refactoring module configuration

--- src/webpack-config/module.js
+++ src/webpack-config/module.js
@@ -136,39 +136,47 @@ const createJsRules = (wizardConfig) => ({
     loader: 'babel-loader',
     options: {
       presets: [
-        require.resolve('@babel/preset-env'),
-        require.resolve('@babel/preset-react'),
-
-        // Stage 0
-        require.resolve('@babel/plugin-proposal-function-bind'),
-
-        // Stage 1
-        require.resolve('@babel/plugin-proposal-export-default-from'),
-        require.resolve('@babel/plugin-proposal-logical-assignment-operators'),
-        [ require.resolve('@babel/plugin-proposal-optional-chaining'), { "loose": false } ],
-        [ require.resolve('@babel/plugin-proposal-pipeline-operator'), { "proposal": "minimal" } ],
-        [ require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'), { "loose": false } ],
-        require.resolve('@babel/plugin-proposal-do-expressions',
-
-        // Stage 2
-        [ require.resolve('@babel/plugin-proposal-decorators'), { "legacy": true } ],
-        require.resolve('@babel/plugin-proposal-function-sent'),
-        require.resolve('@babel/plugin-proposal-export-namespace-from'),
-        require.resolve('@babel/plugin-proposal-numeric-separator'),
-        require.resolve('@babel/plugin-proposal-throw-expressions'),
-
-        // Stage 3
-        require.resolve('@babel/plugin-syntax-dynamic-import'),
-        require.resolve('@babel/plugin-syntax-import-meta'),
-        [ require.resolve('@babel/plugin-proposal-class-properties'), { "loose": false } ],
-        require.resolve('@babel/plugin-proposal-json-strings')
+        require.resolve('@babel/preset-env', { modules: false }),
+        require.resolve('@babel/preset-react')
       ],
       plugins: [
+        ...getBabelStage0Plugins(),
+        ...getBabelStage1Plugins(),
+        ...getBabelStage2Plugins(),
+        ...getBabelStage3Plugins(),
         require.resolve('react-hot-loader/babel')
       ]
     }
   }
 });
+
+const getBabelStage0Plugins = () => [
+  require.resolve('@babel/plugin-proposal-function-bind')
+];
+
+const getBabelStage1Plugins = () => [
+  require.resolve('@babel/plugin-proposal-export-default-from'),
+  require.resolve('@babel/plugin-proposal-logical-assignment-operators'),
+  [ require.resolve('@babel/plugin-proposal-optional-chaining'), { loose: false } ],
+  [ require.resolve('@babel/plugin-proposal-pipeline-operator'), { proposal: 'minimal' } ],
+  [ require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'), { loose: false } ],
+  require.resolve('@babel/plugin-proposal-do-expressions')
+];
+
+const getBabelStage2Plugins = () => [
+  [ require.resolve('@babel/plugin-proposal-decorators'), { legacy: true } ],
+  require.resolve('@babel/plugin-proposal-function-sent'),
+  require.resolve('@babel/plugin-proposal-export-namespace-from'),
+  require.resolve('@babel/plugin-proposal-numeric-separator'),
+  require.resolve('@babel/plugin-proposal-throw-expressions')
+];
+
+const getBabelStage3Plugins = () => [
+  require.resolve('@babel/plugin-syntax-dynamic-import'),
+  require.resolve('@babel/plugin-syntax-import-meta'),
+  [ require.resolve('@babel/plugin-proposal-class-properties'), { loose: false } ],
+  require.resolve('@babel/plugin-proposal-json-strings')
+];
 
 const createIncludePaths = (filepath) => {
   if (Array.isArray(filepath)) {
